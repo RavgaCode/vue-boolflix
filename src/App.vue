bb6587b7fe549678d000286367def9a699f99fd1
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-12 17:07:25 +0200

    repo fix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainContent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainContent from "./components/MainContent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainContent,
+  },
+};
 </script>
 
 <style lang="scss">
+@import "./style/style.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="container">
+      <div>
+        <input type="text" placeholder="Che film stai cercando?" />
+      </div>
+      <div class="row">
+        <div class="col">
+          <h2>Titolo</h2>
+          <h3>Titolo originale</h3>
+          <h4>Lingua</h4>
+          <h5>Voto</h5>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MainContent",
+};
+</script>
+
+<style lang="scss">
+@import "../style/style.scss";
+</style>
