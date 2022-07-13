bf159b45d3a61e7291ab67b83f5fabb1ca3831f5
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-13 18:03:25 +0200

    Series call and cards print

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -11,6 +11,7 @@
       <div>
         <button type="submit" @click="getMovies">Cerca</button>
       </div>
+      <!-- Movie section -->
       <div class="row">
         <div class="col-4" v-for="(movie, index) in movieList" :key="index">
           <h2>{{ movie.title }}</h2>
@@ -31,6 +32,27 @@
           <h5>{{ movie.vote_average }}</h5>
         </div>
       </div>
+      <!-- Series section -->
+      <div class="row">
+        <div class="col-4" v-for="(serie, index) in seriesList" :key="index">
+          <h2>{{ serie.name }}</h2>
+          <h3>{{ serie.original_name }}</h3>
+          <!-- Language flag -->
+          <img
+            v-if="availableFlags.includes(serie.original_language)"
+            :src="require(`../assets/img/${serie.original_language}.svg`)"
+            alt=""
+            class="language-flag"
+          />
+          <img
+            v-else
+            src="../assets/img/euro.svg"
+            alt=""
+            class="language-flag"
+          />
+          <h5>{{ serie.vote_average }}</h5>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -43,6 +65,7 @@ export default {
     return {
       text: "",
       movieList: [],
+      seriesList: [],
       availableFlags: ["en", "it"],
     };
   },
@@ -55,6 +78,14 @@ export default {
         )
         .then((result) => {
           this.movieList = result.data.results;
+        });
+
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=b585fd483e3e78376584f5c26e640090&query=${this.text}`
+        )
+        .then((result) => {
+          this.seriesList = result.data.results;
         });
       // Resetto la searchbar
       this.text = "";
