0c1853399fa2b21068ca15a2f586e7c788524e7c
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-13 20:26:18 +0200

    Milestone-3 Star rating

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,8 +1,10 @@
 <template>
   <section>
     <div class="container">
+      <!-- Searchbar -->
       <div>
         <input
+          @keyup.enter="getMovies()"
           type="text"
           placeholder="Che film stai cercando?"
           v-model="text"
@@ -14,13 +16,14 @@
       <!-- Movie section -->
       <div class="row">
         <div class="col-4" v-for="(movie, index) in movieList" :key="index">
+          <!-- Foto poster del film -->
           <img
             :src="`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`"
             alt=""
           />
           <h2>{{ movie.title }}</h2>
           <h3>{{ movie.original_title }}</h3>
-          <!-- Language flag -->
+          <!-- Language flag con placeholder in caso di bandiera mancante-->
           <img
             v-if="availableFlags.includes(movie.original_language)"
             :src="require(`../assets/img/${movie.original_language}.svg`)"
@@ -33,19 +36,30 @@
             alt=""
             class="language-flag"
           />
-          <h5>{{ movie.vote_average }}</h5>
+          <h5>
+            <!-- Stars rating -->
+            <span
+              v-for="(element, index) in 5"
+              :key="index"
+              :class="{
+                'yellow-star': element <= starsRating(movie.vote_average),
+              }"
+              ><i class="fa-solid fa-star"></i
+            ></span>
+          </h5>
         </div>
       </div>
       <!-- Series section -->
       <div class="row">
         <div class="col-4" v-for="(serie, index) in seriesList" :key="index">
+          <!-- Foto poster della serie -->
           <img
             :src="`https://image.tmdb.org/t/p/w342${serie.backdrop_path}`"
             alt=""
           />
           <h2>{{ serie.name }}</h2>
           <h3>{{ serie.original_name }}</h3>
-          <!-- Language flag -->
+          <!-- Language flag con placeholder in caso di bandiera mancante-->
           <img
             v-if="availableFlags.includes(serie.original_language)"
             :src="require(`../assets/img/${serie.original_language}.svg`)"
@@ -58,7 +72,17 @@
             alt=""
             class="language-flag"
           />
-          <h5>{{ serie.vote_average }}</h5>
+          <h5>
+            <!-- Stars rating -->
+            <span
+              v-for="(element, index) in 5"
+              :key="index"
+              :class="{
+                'yellow-star': element <= starsRating(serie.vote_average),
+              }"
+              ><i class="fa-solid fa-star"></i
+            ></span>
+          </h5>
         </div>
       </div>
     </div>
@@ -87,7 +111,7 @@ export default {
         .then((result) => {
           this.movieList = result.data.results;
         });
-
+      // Ottengo l'array per le serie TV con lo stesso metodo precedente
       axios
         .get(
           `https://api.themoviedb.org/3/search/tv?api_key=b585fd483e3e78376584f5c26e640090&query=${this.text}`
@@ -98,10 +122,9 @@ export default {
       // Resetto la searchbar
       this.text = "";
     },
-    changeLanguage() {
-      this.movieList.forEach((element) => {
-        console.log(element.original_language);
-      });
+    starsRating(number) {
+      let rating = Math.round(number / 2);
+      return rating;
     },
   },
 };
@@ -111,5 +134,8 @@ export default {
 @import "../style/style.scss";
 .language-flag {
   width: 30px;
+}
+.yellow-star {
+  color: yellow;
 }
 </style>
